36154958168d860ffaa9b4a160278ff064dc6f63
BangZ <istobran@gmail.com>
2016-05-09 10:15:29 +0800

    fixed category style

--- archive.php
+++ archive.php
@@ -3,34 +3,35 @@
 <div class="container" id="main">
     <div class="row">
         <div class="col-md-8">
-            <div class="alert alert-success">您正在查看: <?php $this->archiveTitle(array(
+            <div class="alert alert-primary">您正在查看: <?php $this->archiveTitle(array(
                 'category'  =>  _t(' %s 分类下的文章'),
                 'search'    =>  _t('包含关键字 %s 的文章'),
                 'tag'       =>  _t('标签 %s 下的文章'),
                 'author'    =>  _t('%s 发布的文章')
                 ), '', ''); ?></div>
             <?php if ($this->have()): ?>
-                <?php while($this->next()): ?>
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <h3 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h3>
-                        <div class="post-meta">
-                            <span>作者：<a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a> | </span>
-                            <span>时间：<?php $this->date('F j, Y'); ?> | </span>
-                            <span>分类：<?php $this->category(','); ?> | </span>
-                            <span>评论：<a href="<?php $this->permalink() ?>"><?php $this->commentsNum('%d 评论'); ?></a> </span>
-                        </div>
-                        <div class="post-content"><?php $this->content('Continue Reading...'); ?></div>
-                    </div>
-                </div>
-                <?php endwhile; ?>
-                <?php else: ?>
-                    <article class="block">
-                        <h2 class="header"><?php _e('没有找到内容'); ?></h2>
-                    </article>
-                <?php endif; ?>
-
-            <?php $this->pageNav('&laquo; 前一页', '后一页 &raquo;'); ?>
+              <?php while($this->next()): ?>
+                <article class="panel panel-default">
+        					<div class="article-thumbnail">
+        						<div class="post-cover" style="background-image:url(<?php $this->options->themeUrl("img/test-banner.png"); ?>);">
+        							<a href="<?php $this->permalink() ?>"><div class="plus"><img src="<?php $this->options->themeUrl("img/icon-plus.png"); ?>" alt="icon-plus"></div></a>
+        						</div>
+        						<h3 class="post-title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h3>
+        						<p class="post-meta">
+        							<?php $this->date('M j, Y'); ?>
+        						</p>
+        					</div>
+        					<div class="panel-body">
+        						<div class="post-content index-content"><?php $this->content('<button class="btn btn-primary">继续阅读 <i class="fa fa-hand-o-right" aria-hidden="true"></i></button>'); ?></div>
+        					</div>
+        				</article>
+              <?php endwhile; ?>
+            <?php else: ?>
+              <article class="block">
+                  <h2 class="header"><?php _e('没有找到内容'); ?></h2>
+              </article>
+            <?php endif; ?>
+            <?php $this->pageNav('<i class="fa fa-angle-double-left" aria-hidden="true"></i>', '<i class="fa fa-angle-double-right" aria-hidden="true"></i>'); ?>
 
         </div>
         <?php $this->need('sidebar.php'); ?>

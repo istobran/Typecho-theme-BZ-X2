3b6c932bbe5fc3367cd08d76497b1db393908f44
BangZ <istobran@gmail.com>
2016-05-10 15:39:01 +0800

    change commit status

--- functions.php
+++ functions.php
@@ -25,29 +25,13 @@ function themeConfig($form) {
     $miibeian = new Typecho_Widget_Helper_Form_Element_Text('miibeian', NULL, _t('粤ICP备14072384号-2'), _t('备案号'), _t('在这里填入天朝备案号，不显示则留空'));
     $form->addInput($miibeian);
 
+		$cdnurl = new Typecho_Widget_Helper_Form_Element_Text('cdnurl', NULL, _t('静态资源CDN的URL'), _t('CDN的地址'), _t('在这里填入CDN的地址，如七牛、又拍云等'));
+		$form->addInput($cdnurl);
+
     $misc = new Typecho_Widget_Helper_Form_Element_Checkbox('misc', array(
         'ShowLogin' => _t('前台显示登录入口'),
         'ShowLoadTime' => _t('页脚显示加载耗时')
         ),
     array('ShowLogin'), _t('杂项'));
     $form->addInput($misc->multiMode());
-}
-
-function timer_start() {
-    global $timestart;
-    $mtime = explode( ' ', microtime() );
-    $timestart = $mtime[1] + $mtime[0];
-    return true;
-}
-timer_start();
-
-function timer_stop( $display = 0, $precision = 3 ) {
-    global $timestart, $timeend;
-    $mtime = explode( ' ', microtime() );
-    $timeend = $mtime[1] + $mtime[0];
-    $timetotal = $timeend - $timestart;
-    $r = number_format( $timetotal, $precision );
-    if ( $display )
-    echo $r;
-    return $r;
 }
